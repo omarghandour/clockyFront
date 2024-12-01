1bb3d1214fcfa65e1fc988ff32300f69b2d317bf
Omar <omarkhaled9913@gmail.com>
2024-12-01 09:37:22 +0200

    optimize images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    loader: "default", // Use the default loader
+    unoptimized: true, // Disable optimization for static hosting
     remotePatterns: [
       {
         protocol: "https",
