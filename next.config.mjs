c2d0007252f32a61e300f99f2e818f0c6ed3cb31
Omar <omarkhaled9913@gmail.com>
2024-12-01 10:04:10 +0200

    test

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     loader: "default", // Use the default loader
     // unoptimized: true, // Disable optimization for static hosting
+    domains: [
+      "cloud.appwrite.io", // Add the domain of your external images here
+    ],
     remotePatterns: [
       {
         protocol: "https",
