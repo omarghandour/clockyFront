5ca321c852673d3e4787c2f903eff10ed775566e
Omar <omarkhaled9913@gmail.com>
2024-12-26 17:15:04 +0200

    refactor Cart component to include Authorization header for API requests

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -48,10 +48,10 @@ const Cart = () => {
     try {
       const userId = localStorage.getItem("userId");
       const token = localStorage.getItem("token");
-      // if (!userId || !token) return;
+      if (!userId || !token) return;
 
       const { data } = await axiosInstance.get(`/products/cart/${userId}`, {
-        withCredentials: true,
+        headers: { Authorization: `Bearer ${token}` },
       });
 
       const updatedCart = data.map((item: any) => ({
