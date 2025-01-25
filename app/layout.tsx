f7c682bc1d01ca94fd408733dcb0ac2b08748881
Omar <omarkhaled9913@gmail.com>
2025-01-25 16:10:02 +0200

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,9 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import "../public/fonts/style.css";
 import { Toaster } from "@/components/ui/toaster";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/react";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-import CookiesWarning from "./components/CookiesWarning";
+// import CookiesWarning from "./components/CookiesWarning";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -24,28 +22,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" /> */}
-        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
-        {/* <link
-          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-          rel="stylesheet"
-        /> */}
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
       </head>
       <body className={`${inter.className}  bg-main`}>
-        {/* <Nav /> */}
-        {/* <SidebarProvider> */}
-        {/* <SidebarTrigger /> */}
         <Nav />
         {children}
         {/* <CookiesWarning /> */}
         <Toaster />
-        <Analytics />
-        <SpeedInsights />
         <Footer />
         {/* </SidebarProvider> */}
       </body>
