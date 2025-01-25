e3f3c8cb3eb0e3c27edce6ea3027c5a7d4e51d3d
Omar <omarkhaled9913@gmail.com>
2025-01-25 16:06:32 +0200

    card

--- app/components/BrandProducts.tsx
+++ app/components/BrandProducts.tsx
@@ -6,6 +6,7 @@ import { useToast } from "@/hooks/use-toast";
 import Link from "next/link";
 import { Skeleton } from "@/components/ui/skeleton"; // Import Skeleton component
 import { useParams } from "next/navigation";
+import Card from "./Card";
 
 type Product = {
   _id: string;
@@ -34,6 +35,8 @@ const BrandProducts: React.FC = () => {
             withCredentials: true,
           }
         );
+        console.log(response);
+
         setProducts(response.data);
       } catch (error) {
         console.error("Failed to fetch products", error);
@@ -74,7 +77,7 @@ const BrandProducts: React.FC = () => {
   };
 
   return (
-    <div className="flex mt-24 py-10 flex-col items-center h-[100dvh] w-full bg-[#FCFCFC] pt-10 p-2 mx-auto xl:w-3/4">
+    <div className="flex mt-20 py-10 flex-col items-center  w-full bg-[#FCFCFC] pt-10 p-2 mx-auto xl:w-3/4">
       <div className="border-t-2 border-two w-20 p-1 font-medium"></div>
       <h2 className="text-main font-bold text-xl md:text-2xl">
         {brand.toUpperCase()} PRODUCTS
@@ -99,46 +102,7 @@ const BrandProducts: React.FC = () => {
       ) : (
         <div className="grid gap-4 md:gap-8 xl:gap-16 grid-cols-[repeat(auto-fit,minmax(150px,1fr))] place-items-center w-full md:w-4/5">
           {products.map((product) => (
-            <div
-              key={product._id}
-              className="mt-4 md:mt-6 border-solid border-2 border-[#F0F0F0] flex flex-col items-center pb-8 gap-2 shadow-lg transition-transform duration-300 transform md:hover:scale-105 w-full md:w-[225px]"
-            >
-              <Link
-                href={`/product/${product._id}`}
-                className="flex w-full flex-col gap-2 justify-between h-full"
-              >
-                <img
-                  src={product.img}
-                  loading="lazy"
-                  alt={product.name}
-                  className="w-full object-cover h-40 md:h-64"
-                />
-                <div className="text-center">
-                  <h1 className="text-main font-bold text-lg md:text-xl truncate w-full">
-                    {product.name}
-                  </h1>
-                  <p className="text-[#595959] text-sm line-through">
-                    {product.before} L.E
-                  </p>
-                  <p className="text-two font-bold text-xl md:text-2xl">
-                    {product.price} L.E
-                  </p>
-                </div>
-              </Link>
-              <button
-                onClick={() => addToCart(product)}
-                className="relative h-10 flex items-center justify-center px-4 py-2 bg-main text-white font-semibold border overflow-hidden group"
-              >
-                <div
-                  className={`absolute center inset-0 group-hover:translate-x-0 bg-two w-full h-full transform translate-x-full transition-transform duration-500 ease-in-out ${
-                    activeProductId === product._id ? "translate-x-0" : ""
-                  }`}
-                >
-                  <span className="truncate center">ADD TO CART</span>
-                </div>
-                <span className="truncate">ADD TO CART</span>
-              </button>
-            </div>
+            <Card key={product._id} product={product} />
           ))}
         </div>
       )}
