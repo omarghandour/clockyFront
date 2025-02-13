19b1afddc2920818aa69423fa5f281bd081e478e
Omar <omarkhaled9913@gmail.com>
2025-02-13 10:59:42 +0200

    cursol adjust

--- components/full-width-carousel.tsx
+++ components/full-width-carousel.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState, useCallback, useEffect } from "react";
 import useEmblaCarousel from "embla-carousel-react";
-// import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import Link from "next/link";
@@ -17,6 +16,7 @@ const images = [
   "/cover6.png",
   "/cover7.png",
 ];
+
 const smallImages = [
   "/525A0030.jpg",
   "/525A3860.jpg",
@@ -44,29 +44,28 @@ const watchDetails = [
   },
   {
     image: "/525A9327.jpg",
-    name: "AUDEMARS",
-    brand: "Piguet Rubber Chronograph",
+    name: "Audemars Piguet",
+    brand: "Royal Oak Chronograph",
     description:
-      "Elevate your style with the Audemars Piguet Chronograph Olive Green Rubber Strap. A perfect blend of luxury and functionality. Order now and enjoy fast nationwide delivery!",
+      "Discover timeless elegance with the Audemars Piguet Royal Oak Chronograph. Known for its iconic design, impeccable craftsmanship, and precision, this watch is a true symbol of luxury and sophistication. Perfect for those who value both functionality and style, it’s more than a timepiece—it’s a statement. Elevate your collection with this masterpiece today.",
     href: "https://www.clockyeg.com/product/67aa1b51fb260bfb67b99967",
   },
   {
     image: "/525A3860.jpg",
     name: "Rolex DAYTONA",
-    brand: "Piguet Royal",
+    brand: "Panda",
     description:
-      "A sleek and sporty variant of the iconic Daytona, featuring a durable rubber strap for added comfort and style. With its precision chronograph function, tachymetric scale, and robust stainless steel case, it’s the perfect combination of performance, luxury, and modern design.",
+      "Rolex Daytona Rubber – A sleek and sporty variant of the iconic Daytona, featuring a durable rubber strap for added comfort and style. With its precision chronograph function, tachymetric scale, and robust stainless steel case, it’s the perfect combination of performance, luxury, and modern design.",
     href: "https://www.clockyeg.com/product/67a5fd1ffb260bfb67b966f5",
   },
   {
     image: "/525A9327.jpg",
-    name: "ROLEX",
-    brand: "Day-Date",
+    name: "Audemars Piguet",
+    brand: "Royal Oak",
     description:
-      "Discover timeless sophistication with the Rolex Day-Date Arabic Dial. A symbol of prestige and elegance crafted for those who value excellence. Order now and enjoy fast nationwide delivery!",
+      "Elevate your style with the Royal Oak Selfwinding Flying Tourbillon. Featuring a crisp white dial and iconic design, this masterpiece blends elegance with technical excellence. A true symbol of luxury and innovation.",
     href: "",
   },
-
   {
     name: "Watch 6",
     brand: "Brand F",
@@ -90,18 +89,13 @@ export default function FullWidthCarousel() {
     align: "center",
     dragFree: false,
   });
+
   const [prevBtnEnabled, setPrevBtnEnabled] = useState(false);
   const [nextBtnEnabled, setNextBtnEnabled] = useState(false);
   const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-  const scrollPrev = useCallback(
-    () => emblaApi && emblaApi.scrollPrev(),
-    [emblaApi]
-  );
-  const scrollNext = useCallback(
-    () => emblaApi && emblaApi.scrollNext(),
-    [emblaApi]
-  );
+  const scrollPrev = useCallback(() => emblaApi?.scrollPrev(), [emblaApi]);
+  const scrollNext = useCallback(() => emblaApi?.scrollNext(), [emblaApi]);
 
   const onSelect = useCallback(() => {
     if (!emblaApi) return;
@@ -115,16 +109,14 @@ export default function FullWidthCarousel() {
     onSelect();
     emblaApi.on("select", onSelect);
 
-    // Auto-scroll functionality
-    let intervalId = setInterval(() => {
+    const intervalId = setInterval(() => {
       if (emblaApi.canScrollNext()) {
         emblaApi.scrollNext();
       } else {
-        emblaApi.scrollTo(0); // Reset to the first slide
+        emblaApi.scrollTo(0);
       }
-    }, 8000); // Change slide every 3 seconds
+    }, 8000);
 
-    // Cleanup function
     return () => {
       clearInterval(intervalId);
       emblaApi.off("select", onSelect);
@@ -133,10 +125,10 @@ export default function FullWidthCarousel() {
 
   useEffect(() => {
     const handleResize = () => {
-      setIsSmallScreen(window.innerWidth < 768); // Adjust the breakpoint as needed
+      setIsSmallScreen(window.innerWidth < 768);
     };
 
-    handleResize(); // Check on initial render
+    handleResize();
     window.addEventListener("resize", handleResize);
 
     return () => {
@@ -148,11 +140,10 @@ export default function FullWidthCarousel() {
 
   return (
     <div className="relative mt-20 h-full">
-      <div className="overflow-hidden " ref={emblaRef}>
+      <div className="overflow-hidden" ref={emblaRef}>
         <div className="flex h-full">
-          {displayedImages?.map((src, index) => (
+          {displayedImages.map((src, index) => (
             <div key={index} className="relative flex-[0_0_100%] group">
-              {/*  eslint-disable-next-line @next/next/no-img-element */}
               <Image
                 src={src}
                 alt={`Slide ${index + 1}`}
@@ -162,9 +153,9 @@ export default function FullWidthCarousel() {
                 className="w-full h-full object-cover"
               />
               {isSmallScreen && (
-                <div className=" absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-around items-start text-white pl-14 p-4">
+                <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-around items-start text-white pl-14 p-4">
                   <div>
-                    <h1 className=" font-bold text-3xl ">
+                    <h1 className="font-bold text-3xl">
                       {watchDetails[index].name}
                     </h1>
                     <h3 className="text-2xl italic">
@@ -172,14 +163,14 @@ export default function FullWidthCarousel() {
                     </h3>
                   </div>
                   <p className="text-sm">{watchDetails[index].description}</p>
-                  <Link
-                    className={`bg-main p-2 text-two ${
-                      watchDetails[index].href ? "flex" : "hidden"
-                    }`}
-                    href={`${watchDetails[index].href}`}
-                  >
-                    Shop Now
-                  </Link>
+                  {watchDetails[index].href && (
+                    <Link
+                      className="bg-main p-2 text-two"
+                      href={watchDetails[index].href}
+                    >
+                      Shop Now
+                    </Link>
+                  )}
                 </div>
               )}
             </div>
