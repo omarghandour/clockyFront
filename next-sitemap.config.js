b86447e6d6f04339e742038b44c7439c61d60368
Omar <omarkhaled9913@gmail.com>
2025-02-13 10:11:08 +0200

    adding sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://clockyeg.com", // Change this to your domain
+  generateRobotsTxt: true, // Automatically generate robots.txt
+  sitemapSize: 5000, // Optional, useful for large sites
+};
