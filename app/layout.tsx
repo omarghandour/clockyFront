85734ca1a6cd9fe69415250ea4e40ed52157cb35
Omar <omarkhaled9913@gmail.com>
2025-02-19 08:52:48 +0200

    facebook pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "../public/fonts/style.css";
 import { Toaster } from "@/components/ui/toaster";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import Script from "next/script";
 // import CookiesWarning from "./components/CookiesWarning";
 const inter = Inter({ subsets: ["latin"] });
 
@@ -56,6 +57,28 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1361227498382033');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1361227498382033&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
       <body className={`${inter.className}  bg-main`}>
         <Nav />
