7d7e5c6753f4b9f60dd5d3742e647ea5be8c78c4
Omar <omarkhaled9913@gmail.com>
2025-02-25 11:11:31 +0200

    making it possible to checkout without login

--- app/components/checkout.tsx
+++ app/components/checkout.tsx
@@ -33,17 +33,17 @@ const Checkout = () => {
   });
   const router = useRouter();
 
-  // Fetch cart from backend using user ID
+  // Fetch cart from backend using user ID or local storage if no token and userId
   useEffect(() => {
     const userToken =
       typeof window !== "undefined" ? localStorage.getItem("token") : null;
 
-    if (userToken) {
-      setToken(userToken);
+    setToken(userToken);
 
-      const userId =
-        typeof window !== "undefined" ? localStorage.getItem("userId") : null;
+    const userId =
+      typeof window !== "undefined" ? localStorage.getItem("userId") : null;
 
+    if (userToken && userId) {
       axiosInstance
         .get(`/products/cart/${userId}`, {
           headers: { Authorization: `Bearer ${userToken}` },
@@ -61,7 +61,15 @@ const Checkout = () => {
           }
         });
     } else {
-      alert("Please log in to view your cart.");
+      // Get cart from local storage if no token and userId
+      const storedCart =
+        typeof window !== "undefined"
+          ? JSON.parse(localStorage.getItem("cart") || "[]")
+          : null;
+      console.log(storedCart);
+
+      setCartItems(storedCart);
+      updateTotalPrice(storedCart);
     }
   }, [router]);
 
@@ -124,8 +132,11 @@ const Checkout = () => {
     e.preventDefault();
 
     try {
+      console.log(cartItems);
+
       const checkoutPayload = {
         userId: localStorage.getItem("userId"),
+        cart: cartItems,
         paymentMethod: userInfo.paymentMethod,
         shippingAddress: {
           fullName: userInfo.fullName,
@@ -150,6 +161,7 @@ const Checkout = () => {
       if (response.status === 201) {
         alert("Checkout completed successfully!");
         setCartItems([]);
+        localStorage.removeItem("cart"); // Clear the cart from local storage
         router.push("/success");
       }
     } catch (error: any) {
@@ -160,212 +172,208 @@ const Checkout = () => {
 
   return (
     <div className="p-4 w-full max-w-md mx-auto bg-white shadow-lg rounded-lg text-main mt-20 md:mt-0">
-      {token ? (
-        <div>
-          <h2 className="text-2xl font-bold mb-4">Checkout</h2>
-          <form onSubmit={handleSubmit}>
-            {/* Full Name */}
-            <div className="mb-4">
-              <label className="block text-sm font-semibold mb-1">
-                First Name
-              </label>
-              <input
-                type="text"
-                name="fullName"
-                value={userInfo.fullName}
-                onChange={handleInputChange}
-                className="w-full p-2 border rounded"
-                required
-              />
-            </div>
-            {/* Last Name */}
-            <div className="mb-4">
-              <label className="block text-sm font-semibold mb-1">
-                Last Name
-              </label>
-              <input
-                type="text"
-                name="lastName"
-                value={userInfo.lastName}
-                onChange={handleInputChange}
-                className="w-full p-2 border rounded"
-                required
-              />
-            </div>
+      {/* {token ? ( */}
+      <div>
+        <h2 className="text-2xl font-bold mb-4">Checkout</h2>
+        <form onSubmit={handleSubmit}>
+          {/* Full Name */}
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-1">
+              First Name
+            </label>
+            <input
+              type="text"
+              name="fullName"
+              value={userInfo.fullName}
+              onChange={handleInputChange}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+          {/* Last Name */}
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-1">
+              Last Name
+            </label>
+            <input
+              type="text"
+              name="lastName"
+              value={userInfo.lastName}
+              onChange={handleInputChange}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
 
-            {/* Governorate */}
-            <div className="mb-4">
-              <label className="block text-sm font-semibold mb-1">
-                Governorate
-              </label>
-              <select
-                name="governorate"
-                value={userInfo.governorate}
-                onChange={handleInputChange}
-                className="w-full p-2 border rounded"
-                required
-              >
-                <option value="">Select Governorate</option>
-                <option value="Cairo">Cairo</option>
-                <option value="Alexandria">Alexandria</option>
-                <option value="Giza">Giza</option>
-                <option value="Sharkia">Sharkia</option>
-                <option value="Dakahlia">Dakahlia</option>
-                <option value="Beheira">Beheira</option>
-                <option value="Qalyubia">Qalyubia</option>
-                <option value="Monufia">Monufia</option>
-                <option value="Gharbia">Gharbia</option>
-                <option value="Kafr El Sheikh">Kafr El Sheikh</option>
-                <option value="Damietta">Damietta</option>
-                <option value="Port Said">Port Said</option>
-                <option value="Ismailia">Ismailia</option>
-                <option value="Suez">Suez</option>
-                <option value="North Sinai">North Sinai</option>
-                <option value="South Sinai">South Sinai</option>
-                <option value="Matrouh">Matrouh</option>
-                <option value="New Valley">New Valley</option>
-                <option value="Red Sea">Red Sea</option>
-                <option value="Luxor">Luxor</option>
-                <option value="Aswan">Aswan</option>
-                <option value="Sohag">Sohag</option>
-                <option value="Qena">Qena</option>
-                <option value="Minya">Minya</option>
-                <option value="Beni Suef">Beni Suef</option>
-                <option value="Faiyum">Faiyum</option>
-                <option value="Asyut">Asyut</option>
-              </select>
-            </div>
-
-            {/* Address */}
-            <div className="mb-4">
-              <label className="block text-sm font-semibold mb-1">
-                Address
-              </label>
-              <input
-                type="text"
-                name="address"
-                value={userInfo.address}
-                onChange={handleInputChange}
-                className="w-full p-2 border rounded"
-                required
-              />
-            </div>
-
-            {/* City */}
-            <div className="mb-4">
-              <label className="block text-sm font-semibold mb-1">City</label>
-              <input
-                type="text"
-                name="city"
-                value={userInfo.city}
-                onChange={handleInputChange}
-                className="w-full p-2 border rounded"
-                required
-              />
-            </div>
-
-            {/* Country */}
-            <div className="mb-4">
-              <label className="block text-sm font-semibold mb-1">
-                Country
-              </label>
-              <input
-                type="text"
-                name="country"
-                value={userInfo.country}
-                readOnly
-                className="w-full p-2 border rounded bg-gray-100"
-              />
-            </div>
-
-            {/* Phone Number */}
-            <div className="mb-4">
-              <label className="block text-sm font-semibold mb-1">
-                Phone Number
-              </label>
-              <input
-                type="tel"
-                name="phoneNumber"
-                value={userInfo.phoneNumber}
-                onChange={handleInputChange}
-                className="w-full p-2 border rounded"
-                required
-              />
-            </div>
-
-            {/* Payment Method */}
-            <div className="mb-4">
-              <label className="block text-sm font-semibold mb-1">
-                Payment Method
-              </label>
-              <select
-                name="paymentMethod"
-                value={userInfo.paymentMethod}
-                onChange={handleInputChange}
-                className="w-full p-2 border rounded"
-                required
-              >
-                <option value="">Select Payment Method</option>
-                <option value="Pay with Card">Credit Card</option>
-                <option value="Cash on Delivery">Cash on Delivery</option>
-              </select>
-            </div>
-
-            {/* Coupon Code */}
-            <div className="mb-4">
-              <label className="block text-sm font-semibold mb-1">
-                Coupon Code
-              </label>
-              <div className="flex gap-2">
-                <input
-                  type="text"
-                  value={couponCode?.toLowerCase()}
-                  onChange={(e) => setCouponCode(e.target.value)}
-                  className="w-full p-2 border rounded"
-                  placeholder="Enter coupon code"
-                />
-                <button
-                  type="button"
-                  onClick={handleCouponApply}
-                  className="p-2 bg-main text-two font-semibold rounded hover:bg-two hover:text-main"
-                >
-                  Apply
-                </button>
-              </div>
-              {discountAmount > 0 && (
-                <p className="text-main text-sm mt-1">
-                  Discount applied: -${discountAmount.toFixed(2)}
-                </p>
-              )}
-              {couponError && (
-                <p className="text-red-500 text-sm mt-1">{couponError}</p>
-              )}
-            </div>
-
-            {/* Total Price */}
-            <div className="mb-4">
-              <h3 className="text-lg font-semibold">
-                Price: {discountedPrice || totalPrice} LE + 100 LE Shipping
-              </h3>
-              <p className="text-main text-sm mt-1">
-                Total {(discountedPrice || totalPrice) + 100} LE
-              </p>
-            </div>
-            {checkoutError && (
-              <p className="text-red-500 text-sm mt-4">{checkoutError}</p>
-            )}
-            {/* Submit Button */}
-            <button
-              type="submit"
-              className="w-full py-2 bg-main text-two font-semibold rounded hover:bg-two hover:text-main"
+          {/* Governorate */}
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-1">
+              Governorate
+            </label>
+            <select
+              name="governorate"
+              value={userInfo.governorate}
+              onChange={handleInputChange}
+              className="w-full p-2 border rounded"
+              required
             >
-              Proceed to Checkout
-            </button>
-          </form>
-        </div>
-      ) : (
-        <Login />
-      )}
+              <option value="">Select Governorate</option>
+              <option value="Cairo">Cairo</option>
+              <option value="Alexandria">Alexandria</option>
+              <option value="Giza">Giza</option>
+              <option value="Sharkia">Sharkia</option>
+              <option value="Dakahlia">Dakahlia</option>
+              <option value="Beheira">Beheira</option>
+              <option value="Qalyubia">Qalyubia</option>
+              <option value="Monufia">Monufia</option>
+              <option value="Gharbia">Gharbia</option>
+              <option value="Kafr El Sheikh">Kafr El Sheikh</option>
+              <option value="Damietta">Damietta</option>
+              <option value="Port Said">Port Said</option>
+              <option value="Ismailia">Ismailia</option>
+              <option value="Suez">Suez</option>
+              <option value="North Sinai">North Sinai</option>
+              <option value="South Sinai">South Sinai</option>
+              <option value="Matrouh">Matrouh</option>
+              <option value="New Valley">New Valley</option>
+              <option value="Red Sea">Red Sea</option>
+              <option value="Luxor">Luxor</option>
+              <option value="Aswan">Aswan</option>
+              <option value="Sohag">Sohag</option>
+              <option value="Qena">Qena</option>
+              <option value="Minya">Minya</option>
+              <option value="Beni Suef">Beni Suef</option>
+              <option value="Faiyum">Faiyum</option>
+              <option value="Asyut">Asyut</option>
+            </select>
+          </div>
+
+          {/* Address */}
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-1">Address</label>
+            <input
+              type="text"
+              name="address"
+              value={userInfo.address}
+              onChange={handleInputChange}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+
+          {/* City */}
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-1">City</label>
+            <input
+              type="text"
+              name="city"
+              value={userInfo.city}
+              onChange={handleInputChange}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+
+          {/* Country */}
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-1">Country</label>
+            <input
+              type="text"
+              name="country"
+              value={userInfo.country}
+              readOnly
+              className="w-full p-2 border rounded bg-gray-100"
+            />
+          </div>
+
+          {/* Phone Number */}
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-1">
+              Phone Number
+            </label>
+            <input
+              type="tel"
+              name="phoneNumber"
+              value={userInfo.phoneNumber}
+              onChange={handleInputChange}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+
+          {/* Payment Method */}
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-1">
+              Payment Method
+            </label>
+            <select
+              name="paymentMethod"
+              value={userInfo.paymentMethod}
+              onChange={handleInputChange}
+              className="w-full p-2 border rounded"
+              required
+            >
+              <option value="">Select Payment Method</option>
+              <option value="Pay with Card">Credit Card</option>
+              <option value="Cash on Delivery">Cash on Delivery</option>
+            </select>
+          </div>
+
+          {/* Coupon Code */}
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-1">
+              Coupon Code
+            </label>
+            <div className="flex gap-2">
+              <input
+                type="text"
+                value={couponCode?.toLowerCase()}
+                onChange={(e) => setCouponCode(e.target.value)}
+                className="w-full p-2 border rounded"
+                placeholder="Enter coupon code"
+              />
+              <button
+                type="button"
+                onClick={handleCouponApply}
+                className="p-2 bg-main text-two font-semibold rounded hover:bg-two hover:text-main"
+              >
+                Apply
+              </button>
+            </div>
+            {discountAmount > 0 && (
+              <p className="text-main text-sm mt-1">
+                Discount applied: -${discountAmount.toFixed(2)}
+              </p>
+            )}
+            {couponError && (
+              <p className="text-red-500 text-sm mt-1">{couponError}</p>
+            )}
+          </div>
+
+          {/* Total Price */}
+          <div className="mb-4">
+            <h3 className="text-lg font-semibold">
+              Price: {discountedPrice || totalPrice} LE + 100 LE Shipping
+            </h3>
+            <p className="text-main text-sm mt-1">
+              Total {(discountedPrice || totalPrice) + 100} LE
+            </p>
+          </div>
+          {checkoutError && (
+            <p className="text-red-500 text-sm mt-4">{checkoutError}</p>
+          )}
+          {/* Submit Button */}
+          <button
+            type="submit"
+            className="w-full py-2 bg-main text-two font-semibold rounded hover:bg-two hover:text-main"
+          >
+            Proceed to Checkout
+          </button>
+        </form>
+      </div>
+      {/* // ) : ( */}
+      {/* <Login /> */}
+      {/* // )} */}
     </div>
   );
 };
